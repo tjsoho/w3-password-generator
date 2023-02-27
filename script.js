3d050aa04658e9e278343740cb6a0b4702879370
Tjcarroll1 <tjcarroll1@icloud.com>
2023-02-27 11:52:23 +1000

    added var an if statement to store the value of passwordlength

--- script.js
+++ script.js
@@ -8,7 +8,13 @@ var passwordLength = 8 || 128
 function generatePassword () {
   // 
   prompt("Choose a number between 8 and 128 for the length of your password.");
-  
+    var length = parseInt(prompt('How many characters would you like your password to contain?'));
+    // If user input is less than 8 or greater than 128 send a prompt message to let them know.
+    if (passwordLength < 8 || passwordLength > 128)
+    isNaN(passwordLength)
+    alert('Oh no! You chose a number less than 8 or greater than 128')
+    prompt("Choose a number between 8 and 128 for the length of your password.");
+
   prompt('Would you like your password to contain special characters?  e.g' + '       ' + specialCharacter);
   
   prompt('Would you like your password to contain a number?  e.g' + '       ' + number); 
