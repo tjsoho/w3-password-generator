553cfb166bd97847b49aab10bada1887bbf7047e
Tjcarroll1 <tjcarroll1@icloud.com>
2023-02-27 11:30:56 +1000

    started adding some prompts to the code

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+var lowerCase = ('abcdefghijklmnopqrstuvwxyz')
+var upperCase = ('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+var number = ('0123456789')
+var specialCharacter = ('!@#$%^&()*')
+var passwordLength = 8 || 128
+
+
+function generatePassword () {
+  // 
+  prompt("Choose a number between 8 and 128 for the length of your password.");
+  
+  prompt('Would you like your password to contain special characters?  e.g' + '       ' + specialCharacter);
+  
+  prompt('Would you like your password to contain a number?  e.g' + '       ' + number); 
+
+  prompt('Would you like your password to contain lower case letters?  e.g ' + '       ' + lowerCase);
+  
+  prompt('Would you like your password to contain upper case letters?  e.g ' + '       ' + upperCase);
+}
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -10,5 +29,11 @@ function writePassword() {
 
 }
 
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+
