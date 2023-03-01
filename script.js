223ee2db751d1efbcaf1ed33d5ebe0f6ff9fd90e
Tjcarroll1 <tjcarroll1@icloud.com>
2023-03-01 15:22:52 +1000

    added a variable for the password length chosen by the user and also a variable for the random code to use later

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'
 var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
 var specialCharacterArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=',]
 var passwordLengthArray = 8 || 128
-
+var random = function(){ Math.floor(Math.random() * available.length)};
+console.log('random worked')
 
 function generatePassword () {
   var available = []  
@@ -37,6 +38,9 @@ function generatePassword () {
       available = available.concat(upperCaseArray)
     }
     
+    var userLength = passwordLength
+    console.log(userLength)
+
 }
 
 
