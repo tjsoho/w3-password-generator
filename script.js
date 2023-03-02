0fda7576a10ffabed0f3fa290f831a9478fe3d79
Tjcarroll1 <tjcarroll1@icloud.com>
2023-03-02 10:37:03 +1000

    adding an if statement for all false conditions

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  // The below code is what outputs the random password
+  passwordText.value = password;
+  
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 var lowerCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',]
 var upperCaseArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',]
 var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]
 var specialCharacterArray = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=',]
 var passwordLengthArray = 8 || 128
-var random = function(){ Math.floor(Math.random() * available.length)};
-console.log('random worked')
 
 function generatePassword () {
   var available = []  
@@ -38,32 +51,27 @@ function generatePassword () {
       console.log(upperCaseResponse)
       available = available.concat(upperCaseArray)
     }
+
+  if (
+      specialCharacterResponse == false
+      &&
+      numberResponse == false
+      &&
+      lowerCaseResponse == false
+      &&
+      upperCaseResponse == false
+    ){
+      alert('Whoops! Please make sure you choose at least 1 of the criteria options')
+    }
+      
     
  var userLength = passwordLength
     
- var userPassword = '';
-    for(var i=0; i<passwordLength; i++){
-    userPassword += available.charAt(Math.floor(Math.random() * available.length))
-    console.log(userPassword)
-    }
+ let userPassword = "";
+ for (var i = 0; i < passwordLength; i++) {
+   let random = Math.floor(Math.random() * available.length);
+   userPassword += available[random];
+ }
 
   return userPassword;
 }
-
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  // The below code is what outputs the random password
-  passwordText.value = password;
-  
-}
-
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
